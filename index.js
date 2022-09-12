3d07410fc582f889bd016c03c3b4fbdf816e6ce8
Pamela <pamelafumagalli85@gmail.com>
2022-09-12 20:08:05 -0300

    maquetado y funcionamiento nav

---
+++ index.js
@@ -0,0 +1,17 @@
+// VARIABLES
+const menuMobile = document.getElementById("icono-burger");
+const menuSeccion = document.getElementById("menu-mobile");
+const links = document.querySelectorAll("#menu-mobile li");
+
+// DESPLIEGA O CIERRA MENÚ
+menuMobile.onclick = () => {
+  menuSeccion.classList.toggle("menu-desplegado");
+};
+
+// CIERRA MENÚ AL IR A LINK
+for (let link of links) {
+  link.onclick = () => {
+    menuSeccion.classList.remove("menu-desplegado");
+  };
+}
+
